28fdf7dae773e3809cd529464ce48899d050c9bf
Nikita <120nik01@mail.ru>
2021-12-14 20:24:51 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,54 @@
+"use strict";
+/*
+let number = 4.6; //это числовой тип данных
+
+console.log(-4/0);
+console.log('string' * 9);//NaN (Not a Number)
+
+//console.log(something); тип данных null
+
+//let und;
+//console.log(und); тип данных undefined
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.name);
+//console.log(obj['name']);
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp', 6, {}, []];
+console.log(arr[1]);
+*/
+
+//раздел 7
+
+/*alert('Hello');
+const result = confirm("Are you here?");
+console.log(result);
+
+const answer = prompt("Вам есть 18?", "");
+console.log(typeof(answer));
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваша фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+console.log(typeof(answers));
+*/
+
+//Раздел 8
+
+
+let incr = 10,
+    decr = 10,
+    incr1 = 10,
+    decr1 = 10;
+
+console.log(++incr1);
+console.log(--decr1);
+console.log(incr++);
+console.log(decr--);
