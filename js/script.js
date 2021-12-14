c43a72b413c1f1d727306995ce99490f2ff3163c
Nikita <120nik01@mail.ru>
2021-12-14 23:10:08 +0300

    add lection and script.js with practic

--- js/script.js
+++ js/script.js
@@ -1,54 +1,23 @@
 "use strict";
-/*
-let number = 4.6; //это числовой тип данных
 
-console.log(-4/0);
-console.log('string' * 9);//NaN (Not a Number)
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
 
-//console.log(something); тип данных null
-
-//let und;
-//console.log(und); тип данных undefined
-
-const obj = {
-    name: "John",
-    age: 25,
-    isMarried: false
+const personalMovDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
 
-console.log(obj.name);
-//console.log(obj['name']);
-let arr = ['plum.png', 'orange.jpg', 'apple.bmp', 6, {}, []];
-console.log(arr[1]);
-*/
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-//раздел 7
+personalMovDB.movies[a] = b;
+personalMovDB.movies[c] = d;
 
-/*alert('Hello');
-const result = confirm("Are you here?");
-console.log(result);
-
-const answer = prompt("Вам есть 18?", "");
-console.log(typeof(answer));
-
-const answers = [];
-
-answers[0] = prompt('Как ваше имя?', '');
-answers[1] = prompt('Как ваша фамилия?', '');
-answers[2] = prompt('Сколько вам лет?', '');
-
-console.log(typeof(answers));
-*/
-
-//Раздел 8
+console.log(personalMovDB);
 
 
-let incr = 10,
-    decr = 10,
-    incr1 = 10,
-    decr1 = 10;
-
-console.log(++incr1);
-console.log(--decr1);
-console.log(incr++);
-console.log(decr--);
