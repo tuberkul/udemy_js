e42502cfa921b12f86092eb7d4c86937b1896781
Nikita <120nik01@mail.ru>
2021-12-16 22:18:10 +0300

    add objects
    about destructurization of objects

--- js/lection.js
+++ js/lection.js
@@ -131,3 +131,40 @@ console.log(typeof(answers));
 
 // LearnJS('JavaScript', done);
 
+//урок про объекты
+
+// const options = {
+//   name: 'test',
+//   width: 1024,
+//   height: 1024,
+//   colors: {
+//     border: 'black',
+//     bg: 'red'
+//   },
+//   makeTest: function(){
+//     console.log("Test");
+//   }
+// };
+
+// delete options.name;
+
+// console.log(options);
+
+// for (let key in options) {
+//   if (typeof(options[key]) === 'object'){
+//     for (let i in options[key]){
+//       console.log(`Своиство ${i} имеет значение ${options[key][i]}`);
+//     }
+//   } else {
+//   console.log(`Своиство ${key} имеет значение ${options[key]}`);
+//   }
+// }
+
+//console.log(Object.keys(options).length); //длинна объекта
+
+//options.makeTest();//создание и вызов метода
+
+//const {border, bg} = options.colors; //деструктуризация объектов
+
+//console.log(border);
+
