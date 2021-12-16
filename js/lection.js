c52c42c72df8aedb6e101151f976446a14b351bc
Nikita <120nik01@mail.ru>
2021-12-16 23:48:42 +0300

    transmission by reference or by value and Spread operator

--- js/lection.js
+++ js/lection.js
@@ -200,3 +200,101 @@ console.log(typeof(answers));
 //products.sort();
 //console.log(products.join('; '));
 
+//передача по ссылке или по значению; Spread оператор
+
+// let a = 5,
+//     b = a;
+
+// b = b + 5;
+
+// const obj = {
+//   a: 5,
+//   b: 1
+// };
+
+// const copy = obj; // передает ссылка на объект и при редактировании копии, 
+//                   //редактируется оригинальныи объект
+
+// copy.a = 10;
+
+// console.log(copy);
+// console.log(obj);
+
+function copy(mainObj) {
+  let objCopy = {};
+
+  let key;
+  for (key in mainObj){
+    objCopy[key] = mainObj[key];
+  }
+
+  return objCopy;
+}
+
+const numbers = {
+  a: 2,
+  b: 5,
+  c: {
+    x: 7,
+    y: 4
+  }
+};
+
+const newNumbers = copy(numbers);
+
+newNumbers.a = 10;
+newNumbers.c.x = 10;
+
+//console.log(number);
+//console.log(newNumbers); //это поверностная копия объектов, то есть на первом уровне
+
+const add = {
+  d: 17,
+  e: 20
+};
+
+const clone = Object.assign({}, add); 
+
+clone.d = 20;
+
+// console.log(add);
+// console.log(clone);
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+
+newArray[1] = 'furbv';
+
+console.log(newArray);
+console.log(oldArray);
+
+//Spread
+
+const video = ['youtube', 'vimeo', 'rutube'],
+      blogs = ['wordpress', 'livejournal', 'blogger'],
+      internet = [...video, ...blogs, 'vk', 'instagram', 'facebook'];
+
+console.log(internet);
+
+function log(a, b, c){
+  console.log(a);
+  console.log(b);
+  console.log(c);
+}
+
+const num = [2, 5, 7];
+
+log(...num);
+
+const array = ['a', 'b'];
+
+const newAarray = [...array];
+
+const a = {
+  one: 1,
+  two :2
+};
+
+const newA = {...a};//создание копии для объектов и массивов с помощью
+                    //spread оператора: ...
+
