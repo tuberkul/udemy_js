144c77acb9e110e812c474fb676ec8b2b29562ae
Nikita <120nik01@mail.ru>
2021-12-28 02:55:14 +0300

    add classes

--- this/script.js
+++ this/script.js
@@ -1,4 +1,4 @@
-"use strict";
+//"use strict";
 
 // function showThis() {
 //     console.log(this);
@@ -68,23 +68,60 @@
 
 //теперь немного практики
 
-const btn = document.querySelector('button');
+// const btn = document.querySelector('button');
 
-btn.addEventListener('click', function(){
-    this.style.backgroundColor = 'red';
-});
+// btn.addEventListener('click', function(){
+//     this.style.backgroundColor = 'red';
+// });
 
-const obj = {
-    num: 5,
-    sayNumber: function(){
-        const say = () => {
-            console.log(this);
-        };
-        say();
+// const obj = {
+//     num: 5,
+//     sayNumber: function(){
+//         const say = () => {
+//             console.log(this);
+//         };
+//         say();
+//     }
+// };
+
+// obj.sayNumber();
+
+// const double = a => a * 2; //синтаксис стрелочнои функции
+
+//классы
+
+"use strict";
+
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
     }
-};
 
-obj.sayNumber();
+    calcArea() {
+        return this.height * this.width;
+    }
+}
 
-const double = a => a * 2; //синтаксис стрелочнои функции
+class ColoredRectangleWithText extends Rectangle{
+    constructor(height, width, text, bgColor){
+        super(height, width);
+        this.text = text;
+        this.bgColor = bgColor;
+    }
+
+    showMyProps(){
+        console.log(`Текст: ${this.text}, цвет: ${this.bgColor}`);
+    }
+}
+
+const div = new ColoredRectangleWithText(25, 10, 'Hello world', 'red');
+
+div.showMyProps();
+console.log(div.calcArea());
+// const square = new Rectangle(10, 10);
+// const long = new Rectangle(20, 100);
+
+// console.log(long.calcArea());
+// console.log(square.calcArea());
 
