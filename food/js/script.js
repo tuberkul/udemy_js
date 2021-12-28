773b7bfc198ec39c577b9a812693ff0b2d6e6640
Nikita <120nik01@mail.ru>
2021-12-28 18:03:09 +0300

    add work with ...rest

--- food/js/script.js
+++ food/js/script.js
@@ -145,13 +145,14 @@ window.addEventListener('DOMContentLoaded', () => {
 //используем классы для карточек
 
     class MenuEveryDay {
-        constructor(imgSrc,menuType, title, description, total, parentSelector) {
+        constructor(imgSrc,menuType, title, description, total, parentSelector, ...classes) {
             this.imgSrc = imgSrc;
             this.menuType = menuType;
             this.title = title;
             this.description = description;
             this.cost = 'Цена:';
             this.total = total;
+            this.classes = classes;
             this.parent = document.querySelector(parentSelector);
             this.transfer = 27;
             this.changeToUAH();
@@ -163,8 +164,15 @@ window.addEventListener('DOMContentLoaded', () => {
 
         render() {
             const element = document.createElement('div');
+
+            if (this.classes.length == 0) {
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+
             element.innerHTML = `
-            <div class="menu__item">
                 <img src="${this.imgSrc}" alt="${this.menuType}">
                 <h3 class="menu__item-subtitle">${this.title}</h3>
                 <div class="menu__item-descr">${this.description}</div>
@@ -173,7 +181,6 @@ window.addEventListener('DOMContentLoaded', () => {
                     <div class="menu__item-cost">${this.cost}</div>
                     <div class="menu__item-total"><span>${this.total}</span> грн/день</div>
                 </div>
-            </div>
             `;
 
             this.parent.append(element);
@@ -198,7 +205,8 @@ window.addEventListener('DOMContentLoaded', () => {
         'Меню "Премиум"', 
         descriptionMenuTwo, 
         20,
-        '.menu .container') .render();
+        '.menu .container'
+        ).render();
 
     new MenuEveryDay(
         'img/tabs/post.jpg',
