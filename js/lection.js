0ccfd8dfb4903fbbb29fff51b370a4cce522ba04
Nikita <120nik01@mail.ru>
2022-01-06 07:27:48 +0300

    add massive sort

--- js/lection.js
+++ js/lection.js
@@ -302,7 +302,7 @@ console.log(typeof(answers));
 
 //console.dir([1,2,3]);//экземпляр массива
 
-"use strict";
+//"use strict";
 
 //const soldier = {
 //   health: 400,
@@ -375,4 +375,60 @@ console.log(typeof(answers));
 //3)
 //console.log(typeof(!!"44444"));
 
+
+//методы перебора массивов
+
+//1) filter
+
+'use strict';
+
+// const names = ['Ivan', 'Ann', 'Ksenya', 'Voldemart'];
+
+// const shortNames = names.filter(function(name) {
+//     return name.length < 5;
+// });
+
+// console.log(shortNames);
+
+//2) map трансформирующий метод
+
+// const answers = ['IvAn', 'AnNa', 'Hello'];
+
+// const result = answers.map(item => item.toLowerCase());
+
+// console.log(result);
+
+//3) every/some
+
+// const some = [4, 'qwe', 'stbfd'];
+
+// // console.log(some.some(item => typeof(item) === 'number'));
+
+// console.log(some.every(item => typeof(item) === 'number'));
+
+//reduce
+
+// const arr = [4, 5, 1, 3, 2, 6];
+
+// const res = arr.reduce((sum, current) => sum + current);
+
+// console.log(res);
+
+// const arr = ['apple', 'pear', 'plum'];
+
+// const res = arr.reduce((sum, current) => `${sum}, ${current}`);
+// console.log(res);
+
+const obj = {
+    ivan: 'persone',
+    ann: 'persone',
+    dog: 'animal',
+    cat: 'animal'
+};
+
+const newArr = Object.entries(obj)
+.filter(item => item[1] === 'persone')
+.map(item => item[0]);
+
+console.log(newArr);
 
