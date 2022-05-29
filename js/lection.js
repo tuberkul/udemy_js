2c3bcfc76f48efab0e60cfbce603857f8efd0253
Nikita <120nik01@mail.ru>
2022-05-29 22:39:19 +0300

    инкапсуляция

--- js/lection.js
+++ js/lection.js
@@ -572,3 +572,69 @@ pow(2, 1); //2
 pow(2, 2); //4
 pow(2, 3); //8
 
+
+//Инкапсуляция - один из принципов ООП - хранение объекта в приватном порядке (пользователь не сможет вмешаться и изменить код)
+
+// function User(name, age) {
+//   this.name = name;
+//   let userAge = age;
+
+//   this.say = function() {
+//     console.log(`Имя пользователя: ${this.name}, возраст ${userAge}`);
+//   };
+
+//   this.getAge = function() {
+//     return userAge;
+//   };
+
+//   this.setAge = function(age) {
+//     if (typeof age === 'number' && age > 0 && age < 110) {
+//       userAge = age;
+//     } else {
+//       console.log('Недопустимое значение!');
+//     }
+//   }
+// }
+
+// const ivan = new User('Ivan', 27);
+// console.log(ivan.name);
+// console.log(ivan.getAge());
+
+
+
+// ivan.setAge(30);
+// ivan.setAge(300);
+// console.log(ivan.getAge());
+// ivan.say();
+
+//то что выше - инкапсуляция на примере конструктора, а ниже на примере класса
+
+class User {
+  constructor(name, age) {
+    this.name = name;
+    this._age = age;
+  }
+
+  say() {
+    console.log(`Имя пользователя: ${this.name}, возраст ${this._age}`);
+  }
+
+  get age() {
+    return this._age;
+  }
+
+  set age(age) {
+    if (typeof age === 'number' && age > 0 && age < 110) {
+      this._age = age;
+    } else {
+      console.log('Недопустимое значение!');
+    }
+  }
+}
+
+const vanya = new User('Ivan', 27);
+console.log(vanya.age);
+vanya.age = 150;
+console.log(vanya.age);
+
+vanya.say();
