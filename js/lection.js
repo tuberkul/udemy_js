b002e566f95f06fa73d4a2ac41e0cfe02de3ff62
Nikita <120nik01@mail.ru>
2022-05-30 06:12:22 +0300

    функции генераторы

--- js/lection.js
+++ js/lection.js
@@ -638,3 +638,35 @@ vanya.age = 150;
 console.log(vanya.age);
 
 vanya.say();
+
+
+
+//функции генераторы
+
+// function* generator() {
+//   yield 'S';
+//   yield 'c';
+//   yield 'r';
+//   yield 'i';
+//   yield 'p';
+//   yield 't';
+// }
+
+// const str = generator();
+
+// console.log(str.next().value);
+
+// function* count(n) {
+//   for (let i = 0; i < n; i++) {
+//     yield i;
+//   }
+// }
+
+// // const counter = count(7);
+// // console.log(counter.next().value);
+// // console.log(counter.next().value);
+// // console.log(counter.next().value);
+
+// for (let k of count(7)) {
+//   console.log(k);
+// }
