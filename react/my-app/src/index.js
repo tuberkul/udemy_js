fd654d37f446be2a6d7b3c732bc08983837e6886
Nikita <120nik01@mail.ru>
2022-05-31 22:09:20 +0300

    JSX
    начало работы с JSX

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -2,16 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const text = 'Hello World'
+
+const elem = (
+  <div>
+    <h2 className='text' >Текст: {text}</h2>
+    <input type = "text" name='textPole'/>
+    <label htmlFor="textPole">tyt text</label>
+    <button tabIndex={0} >Click</button>
+  </div>
+);  //вариант с JSX
+
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  elem
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
